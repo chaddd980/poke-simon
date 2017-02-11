1d0a3ce61b5ed5def17140f18b0c350153a668e1
chadd williams <chadd980@hotmail.com>
2017-02-11 07:57:41 -0500

    added background and some styling

--- src/App.js
+++ src/App.js
@@ -9,12 +9,29 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Pokemon audio={new Audio()} pokemon={"http://res.cloudinary.com/dk5ge9sgn/image/upload/v1486798282/007Squirtle_XY_anime_jdl1ze.png"} sound={squirt} />
-        <Pokemon audio={new Audio()} pokemon={"http://res.cloudinary.com/dk5ge9sgn/image/upload/v1486798266/001Bulbasaur_Dream_rsskjs.png"} sound={bulb} />
-        <Pokemon audio={new Audio()} pokemon={"http://res.cloudinary.com/dk5ge9sgn/image/upload/v1486798273/004Charmander_OS_anime_rotmth.png"} sound={char} />
-        <Pokemon audio={new Audio()} pokemon={"http://res.cloudinary.com/dk5ge9sgn/image/upload/v1486798367/025Pikachu_XY_anime_3_zvg897.png"} sound={pika} />
+        <Header />
+        <div className="main-div">
+          <Pokemon className="squirt" audio={new Audio()} pokemon={"http://res.cloudinary.com/dk5ge9sgn/image/upload/v1486798282/007Squirtle_XY_anime_jdl1ze.png"} sound={squirt} />
+          <div className="filler"></div>
+          <div className="middle-pokemon">
+            <Pokemon audio={new Audio()} pokemon={"http://res.cloudinary.com/dk5ge9sgn/image/upload/v1486798266/001Bulbasaur_Dream_rsskjs.png"} sound={bulb} />
+            <Pokemon audio={new Audio()} pokemon={"http://res.cloudinary.com/dk5ge9sgn/image/upload/v1486798273/004Charmander_OS_anime_rotmth.png"} sound={char} />
+          </div>
+          <div className="filler"></div>
+          <Pokemon className="pika" audio={new Audio()} pokemon={"http://res.cloudinary.com/dk5ge9sgn/image/upload/v1486798367/025Pikachu_XY_anime_3_zvg897.png"} sound={pika} />
+        </div>
       </div>
     );
+  }
+}
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <h1>Poke-Simon Game</h1>
+      </div>
+    )
   }
 }
 
@@ -27,11 +44,13 @@ class Pokemon extends Component {
 
   render() {
     return (
-      <div className="footer">
+      <div className="pokemon">
         <div onClick={() => this.playCry()}><img className="pic" src={this.props.pokemon} alt="pic of pokemon"/></div>
       </div>
     )
   }
 }
+
+
 
 export default App;
